ef19ca4b492a480ca0fdf2a1543fbb71f62f632f
Johan Brattemark <johan.brattemark@gmail.com>
2017-06-12 13:31:24 -0700

    Refactor Queue class

--- src/Queue.js
+++ src/Queue.js
@@ -1,18 +1,35 @@
+const Stack = require('./Stack');
+
 class Queue {
   constructor() {
-    this._storage = [];
+    this._StackOne = new Stack();
+    this._StackTwo = new Stack();
   }
 
   size() {
-    return this._storage.length;
+    this._StackOne.size();
   }
 
   enqueue(item) {
-    this._storage.push(item);
+    this._StackOne.push(item);
   }
 
   dequeue() {
-    return this._storage.shift();
+    // 1. Pop all from stack 1 to stack 2
+    let s1 = this._StackOne;
+    let s2 = this._StackTwo;
+    while (s1.size() > 0) {
+      s2.push(s1.pop());
+    }
+
+    // 2. Capture top item on stack 2
+    let item = s2.pop();
+
+    // 3. Pop all from stack 2 to stack 1
+    while (s2.size() > 0) {
+      s1.push(s2.pop());
+    }
+    return item;
   }
 }
 
